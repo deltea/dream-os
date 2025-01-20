36493ab0787ab4820add0de995e57f8e7ab0e0f8
deltea <thcheetah777@gmail.com>
2025-01-20 09:27:48 -0800

    add dithering screen transition

--- autoloads/RoomManager/RoomManager.gd
+++ autoloads/RoomManager/RoomManager.gd
@@ -1,9 +1,16 @@
 extends CanvasLayer
 
+@export var transition_time = 0.5
+
 # var room: PackedScene = preload("res://rooms/room.tscn")
+@onready var player: AnimationPlayer = $AnimationPlayer
+@onready var rect: TextureRect = $TextureRect
 
 ## The current loaded room object.
 var current_room: Room
+
+func _ready() -> void:
+	player.speed_scale = 1 / transition_time
 
 ## Change the current room to another room from the name of the room.
 func change_room(room_name: String):
@@ -12,4 +19,8 @@ func change_room(room_name: String):
 		printerr(room_name + " is not a valid room")
 		return
 
+	player.play_backwards("transition")
+
 	get_tree().change_scene_to_packed(scene)
+
+	player.play("transition")
