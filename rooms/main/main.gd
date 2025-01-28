fe74a17d33338a8ebd29eff3dd65e10f481ea3e1
deltea <thcheetah777@gmail.com>
2025-01-27 17:37:20 -0800

    kinda cool ig

--- rooms/main/main.gd
+++ rooms/main/main.gd
@@ -4,7 +4,12 @@ extends Room
 @onready var app_name: RichTextLabel = $AppNameContainer/CenterContainer/AppName
 @onready var app_name_container: NinePatchRect = $AppNameContainer
 
+const app_icon_float_speed = 3.0
+const app_icon_float_magnitude = 5.0
+
 var curr_index: int = 0
+var float_timer = 0.0
+var direction = 1
 
 func _ready() -> void:
 	update_selected_app()
@@ -19,11 +24,26 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("right") or Input.is_action_just_pressed("left"):
 		curr_index = (curr_index + int(x_input)) % apps.get_child_count()
 		if curr_index < 0: curr_index = apps.get_child_count() - 1
+
+		direction = x_input
 		update_selected_app()
 
 	if Input.is_action_just_pressed("a"):
 		RoomManager.change_room(apps.get_child(curr_index).app_name)
 
+	for i in range(apps.get_child_count()):
+		var app = apps.get_child(i) as AppIcon
+		if i == curr_index:
+			# app.position.y = sin(float_timer * app_icon_float_speed) * app_icon_float_magnitude - app_icon_float_magnitude
+			app.position.y = lerp(app.position.y, -16.0, 5.0 * delta)
+			app.rotation_degrees = lerp(app.rotation_degrees, 15.0 * direction, 5.0 * delta)
+		else:
+			app.position.y = lerp(app.position.y, 0.0, 5.0 * delta)
+			app.rotation_degrees = lerp(app.rotation_degrees, 0.0, 5.0 * delta)
+
+	float_timer += delta
+
 func update_selected_app() -> void:
-	app_name.text = "[center][wave freq=3 amp=10]" + apps.get_child(curr_index).app_name
 	print(apps.get_child(curr_index).app_name + " selected")
+	app_name.text = "[center][wave freq=3 amp=10]" + apps.get_child(curr_index).app_name
+	float_timer = app_icon_float_speed
